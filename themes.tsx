42e0ef3b10f136ea7fcc32569a955999d9ede099
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-09-20 11:07:41 +0800

    Refactor step 1 w/ error

--- themes.tsx
+++ themes.tsx
@@ -11,22 +11,28 @@ export default function Themes() {
     <section className='min-h-screen flex flex-col items-center justify-center'>
       <h1 className='text-4xl mb-5 font-semibold'>Themes</h1>
 
-      <ul className='list-disc'>
-        {themeOptions.map((theme, index) => (
-          <li key={index}>
-            <button
-              onClick={() => setSelectedTheme(theme)}
-              className={theme === selectedTheme ? 'font-bold' : ''}
-            >
-              {theme}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <ThemeOptions />
 
       <p className='mt-10'>
         Selected theme: <strong>{selectedTheme}</strong>
       </p>
     </section>
+  );
+}
+
+function ThemeOptions() {
+  return (
+    <ul className='list-disc'>
+      {themeOptions.map((theme, index) => (
+        <li key={index}>
+          <button
+            onClick={() => setSelectedTheme(theme)}
+            className={theme === selectedTheme ? 'font-bold' : ''}
+          >
+            {theme}
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
